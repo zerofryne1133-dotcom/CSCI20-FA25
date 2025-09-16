13f7ebc7189d6e93c9fc50d87e5c4cf068fe53f7
zerofryne1133-dotcom <zerofryne1133@gmail.com>
2025-09-16 20:25:07 +0000

    it 1

---
+++ Project1/project1-1.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main() {
+    int x = 1;
+    bool b = true; 
+    // boolean value
+    std::printf("%i" , x); 
+    // booleans are numbers
+    std::printf ("Hello World");
+    return 0;
+}
